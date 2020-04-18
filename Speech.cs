02351324c0a4db74fb45901eb2054ca35810c237
zdfg2 <zdfg2@n>
2020-04-19 03:49:27 +0800

    can do without INotifyProxxxxxxxx

--- Speech.cs
+++ Speech.cs
@@ -7,24 +7,18 @@ using System.Threading.Tasks;
 
 namespace DataBindingDemo
 {
-    class Speech : INotifyPropertyChanged
+    class Speech
     {
         private string value_;
 
-        public event PropertyChangedEventHandler PropertyChanged;
 
         public string Words { get { return value_; } 
             set
             {
                 value_ = value;
-                OnPropertyChanged("Words");
             } }
 
-        private void OnPropertyChanged(string v)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
-        }
-
+  
         public Speech(string words)
         {
             Words = words;
